bbdd779f52378995773f01d1b4f4934799c0c24c
Pablo Olivera <p.oliverav@gmail.com>
2022-01-18 11:17:33 -0300

    feat: adding apps to plasma installer fix: install go through gvm

--- installs/arch/bare/3-basics.sh
+++ installs/arch/bare/3-basics.sh
@@ -2,8 +2,17 @@
 
 sudo pacman -Syu
 sudo pacman -S base-devel udiskie udisks2 usbutils git \
-    pacman-contrib go delve htop fzf --needed --noconfirm
+    pacman-contrib delve htop fzf --needed --noconfirm
 
+# Install GO
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+gvm install go1.4 -B
+gvm use go1.4
+export GOROOT_BOOTSTRAP=$GOROOT
+gvm install go1.17.6
+gvm use go1.17.6
+
+# Install yay
 echo "Installing yay"
 cd "$HOME" || exit
 git clone https://aur.archlinux.org/yay.git
--- installs/arch/plasma.sh
+++ installs/arch/plasma.sh
@@ -7,3 +7,12 @@ sudo pacman -S plasma-meta \
   kde-utilities-meta \
   kde-accessibility-meta \
   --needed --noconfirm
+
+sudo systemctl enable sddm
+
+sudo pacman -S firewalld vlc \
+  imwheel rclone firefox \
+  exa bat sassc intltool \
+  --needed --noconfirm
+
+yay -S google-chrome 1password --needed --noconfirm
