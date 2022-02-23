6e9e3902357a473055596ffeeb91456cd9e65b98
Pablo Olivera <p.oliverav@gmail.com>
2022-02-23 07:22:35 -0300

    feat: adding fonts

--- installs/arch/scripts/fonts.sh
+++ installs/arch/scripts/fonts.sh
@@ -4,7 +4,7 @@ echo '*** Installing Fonts ***'
 
 sudo pacman -S noto-fonts-emoji ttf-joypixels ttf-roboto-mono --needed --noconfirm
 
-yay -S ttf-twemoji-color --needed --noconfirm
+yay -S ttf-twemoji-color siji-git ttf-unifont --needed --noconfirm
 
 sudo fc-cache -f
 
